c9e96cc381f3955d4459259c2431973a2e0abddc
markhua <huazhi90@hotmail.com>
2015-05-31 10:17:19 +0800

    Remove extra space

--- On The Map/MapConstant.swift
+++ On The Map/MapConstant.swift
@@ -12,13 +12,13 @@ extension MapClient {
     struct Constants {
         
         // MARK: API Key
-        static let ApiKey : String = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
-        static let AppId : String = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let ApiKey: String = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        static let AppId: String = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
         
         // MARK: URLs
-        static let BaseParseURL : String = "https://api.parse.com/"
-        static let BaseUdacityURL : String = "https://www.udacity.com/api/"
-        static let UdacitySignup : String = "https://www.udacity.com/account/auth#!/signup/"
+        static let BaseParseURL: String = "https://api.parse.com/"
+        static let BaseUdacityURL: String = "https://www.udacity.com/api/"
+        static let UdacitySignup: String = "https://www.udacity.com/account/auth#!/signup/"
     }
 
 }
